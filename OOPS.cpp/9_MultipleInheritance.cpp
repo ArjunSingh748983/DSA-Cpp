e446f397f467d7c96ee1d5e223210470a0b150eb
ArjunSingh748983 <singh.arjun2797@gmail.com>
2024-01-10 15:13:01 +0530

    new commit

--- OOPS.cpp/9_MultipleInheritance.cpp
+++ OOPS.cpp/9_MultipleInheritance.cpp
@@ -18,10 +18,11 @@ public:
     int intelligence;
 };
 int main()
-{   child pintu;
+{
+    child pintu;
     // cout<<pintu.weight; // can't tell differnece that which weight is it the dad's or mom's
-    cout<<pintu.mom::weight; // child class inherited mom's weight
-    cout<<pintu.dad::weight; // child class inherited dad's weight
-    cout<<pintu.intelligence<<pintu.height<<pintu.colour<<endl;
+    cout << pintu.mom::weight; // child class inherited mom's weight
+    cout << pintu.dad::weight; // child class inherited dad's weight
+    cout << pintu.intelligence << pintu.height << pintu.colour << endl;
     return 0;
 }
