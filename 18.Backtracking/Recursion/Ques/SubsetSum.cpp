b1ffe6f3d252b8eccb7bed0d5ea20d698bb2c4c2
ArjunSingh748983 <singh.arjun2797@gmail.com>
2024-02-17 11:46:51 +0530

    backtracking updated

--- 18.Backtracking/Recursion/Ques/SubsetSum.cpp
+++ 18.Backtracking/Recursion/Ques/SubsetSum.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    
+void SubsetSum(vector<int> &arr, multiset<int> &ans, int i, int sum)
+{
+    if (i == arr.size()) { // T.C -> O(2^n * logn) and Spcae : O(n) recursive stack space + O(2^n) for set
+        ans.insert(sum);
+        return;
+    }
+    sum += arr[i];
+    SubsetSum(arr, ans, i + 1, sum); // pick  //                                                     SubsetSum(arr,ans,i+1,sum+arr[i]); // other syntax
+    sum -= arr[i];                   // remove the last included sum while backtracking
+    SubsetSum(arr, ans, i + 1, sum); // not pick //                                                  SubsetSum(arr,ans,i+1,sum);
+}
+int main()
+{
+    vector<int> arr{2, 3, 4};
+    multiset<int> ans;
+    SubsetSum(arr, ans, 0, 0);
+    for (auto x : ans) 
+        cout << x << " ";
     return 0;
 }
