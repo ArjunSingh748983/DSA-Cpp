12bc66c43f771e7d8eea18e4fe3d17e87694bbbd
ArjunSingh748983 <singh.arjun2797@gmail.com>
2024-03-14 03:53:56 +0530

    .

--- krishna.c
+++ krishna.c
@@ -1,16 +1,5 @@
-#include <stdio.h>
+# include <stdio.h>
 int main() {
-    int decNum=111434; char hex[9]={' ',' ',' ',' ',' ',' ',' ',' ',' '}; int i=4;
-    while(decNum){
-        int rem=decNum%16;
-        if(rem>=0 && rem<=9) hex[i--]=(char)rem+'0';
-        else if(rem==10) hex[i--]='A';
-        else if(rem==11) hex[i--]='B';
-        else if(rem==12) hex[i--]='C';
-        else if(rem==13) hex[i--]='D';
-        else if(rem==14) hex[i--]='E';
-        else hex[i--]='F';
-        decNum=decNum/16;
-    }
-    for(int i=0;i<5;i++) printf("%c",hex[i]);
+    int s=size
+    return 0;
 }
--- rough.cpp
+++ rough.cpp
@@ -1,23 +1,14 @@
 #include <bits/stdc++.h>
-#include <string.h>
 using namespace std;
 
-int main() {
-    int x=123455; // 4d2
-    char hex[5]={' ',' ',' ',' ','\0'};
-    int i=4;
-    while(x){
-        int rem=x%16;
-        if(rem>=0 and rem<=9) {
-            hex[i--]=char(rem+'0');
-        }
-        else if(rem==10) hex[i--]='A';
-        else if(rem==11) hex[i--]='B';
-        else if(rem==12) hex[i--]='C';
-        else if(rem==13) hex[i--]='D';
-        else if(rem==14) hex[i--]='E';
-        else if(rem==15) hex[i--]='F';
-        x=x/16;
-    }
-    for(int i=0;i<5;i++) cout<<hex[i];
+int main()
+{
+    int n = 8;
+    // float res = sqrt(((n * (n + 1)) / 2));
+    float res=10.345;
+    float dec=res-int(res);
+    cout<<dec;
+    // if(floor(res)==0) cout<<res;
+    // else cout<<-1;
+    return 0;
 }
