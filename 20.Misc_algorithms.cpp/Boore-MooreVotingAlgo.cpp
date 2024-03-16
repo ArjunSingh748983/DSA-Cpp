969444b980900884ec4dc1b695918a4381bf7ba5
ArjunSingh748983 <singh.arjun2797@gmail.com>
2024-03-16 21:00:31 +0530

    .

--- 20.Misc_algorithms.cpp/Boore-MooreVotingAlgo.cpp
+++ 20.Misc_algorithms.cpp/Boore-MooreVotingAlgo.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int majorityElement(vector<int> &nums)
-{
+int majorityElement(vector<int> &nums) {
     int n = nums.size(); // MOORE VOTING ALGO , find element appearing more than n/2 times
     int candidate = nums[0]; // in n/3 vala ques take 4 variable cand1,cand2,vote1,vote2 and iplement the same algo
     int votes = 0;
@@ -14,9 +13,7 @@ int majorityElement(vector<int> &nums)
         }
     }
     int cnt = 0;
-    for (int x : nums){
-        if (x == candidate) cnt++;
-    }
+    for (int x : nums) if (x == candidate) cnt++;
     if (cnt > n / 2) return candidate;
     return -1;
 }
